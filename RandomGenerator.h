553fcac472f1f8bca1d15bb0661e3e6167d5b5b6
KonstantinHrenov-TKI-142 <qrisperzx23@mail.ru>
2026-09-21 16:17:19 +0300

    RandomGenerator.h

---
+++ RandomGenerator.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Generator.h"
+#include <random>
+
+namespace miit::algebra {
+
+    /**
+     * @brief Генератор случайных чисел в диапазоне [min, max]
+     */
+    class RandomGenerator : public Generator {
+    private:
+        std::uniform_int_distribution<int> distribution;
+        std::mt19937 generator;
+
+    public:
+        /**
+         * @brief Конструктор
+         * @param min Минимальное значение
+         * @param max Максимальное значение
+         */
+        RandomGenerator(const int min, const int max);
+
+        /**
+         * @brief Сгенерировать случайное число
+         * @return Случайное число
+         */
+        int generate() override;
+    };
+
+} // namespace miit::algebra
